fc33c9ab76a5a6382e79237479e45d481cd2e4e9
Takeru Sato <type.in.type@gmail.com>
2018-04-25 18:33:25 +0900

    Add Take test

--- src/buffer.rs
+++ src/buffer.rs
@@ -16,3 +16,30 @@ impl<T> Take<T> for VecDeque<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::Take;
+    use std::collections::VecDeque;
+
+    #[test]
+    fn it_should_move_elements_to_passed_buffer() {
+        let mut queue = VecDeque::new();
+        for i in 0..5 {
+            queue.push_back(i);
+        }
+        let mut buffer = Vec::with_capacity(3);
+
+        queue.take(&mut buffer);
+
+        assert_eq!(buffer, vec![0, 1, 2]);
+        assert_eq!(queue.len(), 2);
+
+        let mut buffer = Vec::with_capacity(3);
+
+        queue.take(&mut buffer);
+
+        assert_eq!(buffer, vec![3, 4]);
+        assert_eq!(queue.len(), 0);
+    }
+}
