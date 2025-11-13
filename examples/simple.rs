9600be67899c153edbf92fb8d1633bf6e4579328
Takeru Sato <type.in.type@gmail.com>
2025-11-13 17:51:56 +0900

    chore: remvoe TODO

--- examples/simple.rs
+++ examples/simple.rs
@@ -30,7 +30,6 @@ static POOL: LazyLock<WorkerPool> = LazyLock::new(|| {
 
 fn init() {
     if std::env::var("RUST_LOG").is_err() {
-        // TODO: Audit that the environment access only happens in single-threaded code.
         unsafe { std::env::set_var("RUST_LOG", "debug") };
     }
     pretty_env_logger::init();
